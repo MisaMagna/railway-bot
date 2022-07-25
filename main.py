ec93ec9b6e3ee163bcc8eab6a70ca9cefb316a23
Misael Pereira <misamagna@gmail.com>
2022-07-25 19:57:54 +0200

    Removed Procfile and added new command

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.command()
+async def pong(ctx):
+    await ctx.send("ping")
+
 
 if __name__ == "__main__":
     bot.run(TOKEN)
